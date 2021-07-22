7095e707adc3d844e9dbcb7c5fa1744e2b41f1bd
sasakitomohiro <t0m0.piii.12.07sk@gmail.com>
2021-07-23 07:55:55 +0900

    modify attach and detach logic

--- app/src/main/java/com/github/sasakitomohiro/rvtablayoutmediator/MainActivity.kt
+++ app/src/main/java/com/github/sasakitomohiro/rvtablayoutmediator/MainActivity.kt
@@ -48,7 +48,7 @@ class MainActivity : AppCompatActivity() {
                     tab.text = items[position].toString()
                 }
             }
-        )
+        ).attach()
     }
 }
 
--- rvtablayoutmediator/src/main/java/com/github/sasakitomohiro/rvtablayoutmediator/RvTabLayoutMediator.kt
+++ rvtablayoutmediator/src/main/java/com/github/sasakitomohiro/rvtablayoutmediator/RvTabLayoutMediator.kt
@@ -10,6 +10,9 @@ class RvTabLayoutMediator(
     private val tabDataProvider: TabDataProvider,
     private val tabConfigurationStrategy: RvTabConfigurationStrategy
 ) {
+    var attached = false
+        private set
+
     private var targetPosition = -1
     private var isManualScroll = false
 
@@ -18,59 +21,65 @@ class RvTabLayoutMediator(
     private val layoutManager
         get() = recyclerView.layoutManager as LinearLayoutManager
 
-    init {
+    private val onScrollListener = object : RecyclerView.OnScrollListener() {
+        var prevScrollState = recyclerView.scrollState
+        var currentScrollState = recyclerView.scrollState
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            prevScrollState = currentScrollState
+            currentScrollState = newState
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            val currentFirstVisiblePosition =
+                this@RvTabLayoutMediator.layoutManager.findFirstVisibleItemPosition()
+            if (targetPosition != -1 && targetPosition != currentFirstVisiblePosition && currentScrollState != RecyclerView.SCROLL_STATE_IDLE) return else targetPosition =
+                -1
+            val id = recyclerView.adapter!!.getItemId(currentFirstVisiblePosition)
+            val index = findTabIndexByItemId(id)
+            val tab = tabLayout.getTabAt(index)
+            tabLayout.selectTab(tab)
+        }
+    }
+
+    private val onTabSelectedListener = object : TabLayout.OnTabSelectedListener {
+        override fun onTabReselected(tab: TabLayout.Tab?) {
+        }
+
+        override fun onTabSelected(tab: TabLayout.Tab?) {
+            tab ?: return
+            val id = findItemIdByTabIndex(tab.position)
+            val position = findItemPositionByItemId(id)
+            targetPosition = position
+            smoothScroller.targetPosition = targetPosition
+            layoutManager.startSmoothScroll(smoothScroller)
+        }
+
+        override fun onTabUnselected(tab: TabLayout.Tab?) {
+        }
+    }
+
+    fun attach() {
         require(recyclerView.layoutManager is LinearLayoutManager)
-        with(recyclerView) {
-            addOnScrollListener(
-                object : RecyclerView.OnScrollListener() {
-                    var prevScrollState = recyclerView.scrollState
-                    var currentScrollState = recyclerView.scrollState
-
-                    override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                        prevScrollState = currentScrollState
-                        currentScrollState = newState
-                    }
-
-                    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                        val currentFirstVisiblePosition =
-                            this@RvTabLayoutMediator.layoutManager.findFirstVisibleItemPosition()
-                        if (targetPosition != -1 && targetPosition != currentFirstVisiblePosition && currentScrollState != RecyclerView.SCROLL_STATE_IDLE) return else targetPosition =
-                            -1
-                        val id = adapter!!.getItemId(currentFirstVisiblePosition)
-                        val index = findTabIndexByItemId(id)
-                        val tab = tabLayout.getTabAt(index)
-                        tabLayout.selectTab(tab)
-                    }
+        tabDataProvider.getTabData().forEachIndexed { index, data ->
+            tabLayout.addTab(
+                tabLayout.newTab().apply {
+                    tabConfigurationStrategy.onConfigureTab(this, index)
                 }
             )
         }
-        with(tabLayout) {
-            tabDataProvider.getTabData().forEachIndexed { index, data ->
-                addTab(
-                    newTab().apply {
-                        tabConfigurationStrategy.onConfigureTab(this, index)
-                    }
-                )
-            }
-            addOnTabSelectedListener(
-                object : TabLayout.OnTabSelectedListener {
-                    override fun onTabReselected(tab: TabLayout.Tab?) {
-                    }
+        recyclerView.addOnScrollListener(onScrollListener)
+        tabLayout.addOnTabSelectedListener(onTabSelectedListener)
 
-                    override fun onTabSelected(tab: TabLayout.Tab?) {
-                        tab ?: return
-                        val id = findItemIdByTabIndex(tab.position)
-                        val position = findItemPositionByItemId(id)
-                        targetPosition = position
-                        smoothScroller.targetPosition = targetPosition
-                        layoutManager.startSmoothScroll(smoothScroller)
-                    }
+        recyclerView.scrollToPosition(0)
+        attached = true
+    }
 
-                    override fun onTabUnselected(tab: TabLayout.Tab?) {
-                    }
-                }
-            )
-        }
+    fun detach() {
+        if (!attached) return
+        recyclerView.removeOnScrollListener(onScrollListener)
+        tabLayout.removeOnTabSelectedListener(onTabSelectedListener)
+        attached = false
     }
 
     private fun findTabIndexByItemId(id: Long) =
